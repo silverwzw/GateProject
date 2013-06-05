e1eba50f4bd80de23c4884d4c69a8bc50b2ce86b
Silverwzw <webmaster@silverwzw.com>
2013-06-05 01:56:48 -0400

    generalized

--- GATE-sandbox/src/com/silverwzw/gatesandbox/GateSandBox.java
+++ GATE-sandbox/src/com/silverwzw/gatesandbox/GateSandBox.java
@@ -30,6 +30,7 @@ import java.io.InputStream;
 import java.net.URL;
 
 public class GateSandBox {
+	static boolean debug = false;
   final static class AnnotationComparator implements Comparator<Annotation>{
 	public int compare(Annotation annot, Annotation o) {
 		long r;
@@ -72,6 +73,9 @@ public class GateSandBox {
 		  }
 	  }
 	  
+	  File dir = new File(args[2]);
+      File filelist[] = dir.listFiles();
+      
 	  
 	  Gate.setGateHome(new File("E:/Steven/Life/NCSU/Research/gate"));
 	  Gate.setPluginsHome(new File("E:/Steven/Life/NCSU/Research/gate/plugins"));
@@ -81,11 +85,8 @@ public class GateSandBox {
 	  
 	  Corpus corpus = Factory.newCorpus("GateSandbox Corpus");
 	  
-	  Document doc = new DocumentImpl();
-	  doc.setContent(new DocumentContentImpl("Jessica is a good girl. Orange is of color orange and it's round. Ballons are of various color and most of them are round. There's a red square on the paper."));
-      
+	  
 	  FeatureMap fm = Factory.newFeatureMap();
-	  //fm.put("listsURL", "file:/E:/Steven/Life/NCSU/Research/gate/plugins/ANNIE/resources/gazetteer/colorandshape.def");
 	  fm.put("encoding", "UTF-8");
 	  fm.put("caseSensitive", (Boolean) true);
       ProcessingResource token = (ProcessingResource) Factory.createResource("gate.creole.tokeniser.DefaultTokeniser");
@@ -109,70 +110,79 @@ public class GateSandBox {
       app.add(sspliter);
       app.add(hashGazetteer);
 
-      app.setCorpus(corpus);
-      corpus.add(doc);
-      app.execute();
-      corpus.clear();
 
-      
-      AnnotationSet defaultAnnotset = doc.getAnnotations();
-      Iterator<Annotation> annotItr = defaultAnnotset.iterator();
-	  
-	  SortedSet<Annotation> annotSorted;
-	  annotSorted = new TreeSet<Annotation>(new AnnotationComparator());
-	  
-	  while (annotItr.hasNext()) {
-	  	Annotation annot;
-	  	String mt;
-	   	annot = annotItr.next();
-	   	if (annot.getType().equals("Sentence")) {
-	   		annotSorted.add(annot);
-	   	}
-	   	
-	   	if (annot.getFeatures() == null || annot.getFeatures().get("majorType") == null) {
-	   		continue;
-	   	}
-	   	mt = annot.getFeatures().get("majorType").toString();
-	   	if (mt.equals("A") || mt.equals("B")) {
-	   		annotSorted.add(annot);
-	   	}
-	  }
-	  
-	  System.out.println(annotSorted.size());
-	  Annotation sentence = null;
-	  boolean color=false, shape=false;
-	  for (Annotation a : annotSorted) {
-		  if (a.getType().equals("Sentence")) {
-			  if (sentence != null) {
-				  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {color:" + color + ", shape:" + shape + "}");
+      for (File file : filelist) {
+    	  System.out.println("Processing File:" + file.getName());
+    	  System.out.println();
+    	  
+    	  Document doc = new DocumentImpl();
+    	  doc.setSourceUrl(new URL("file:///" + file.getAbsolutePath()));
+    	  doc.init();
+    	  
+	      app.setCorpus(corpus);
+	      corpus.add(doc);
+	      app.execute();
+	      corpus.clear();
+	      
+	      
+	      AnnotationSet defaultAnnotset = doc.getAnnotations();
+	      Iterator<Annotation> annotItr = defaultAnnotset.iterator();
+		  
+		  SortedSet<Annotation> annotSorted;
+		  annotSorted = new TreeSet<Annotation>(new AnnotationComparator());
+		  
+		  while (annotItr.hasNext()) {
+		  	Annotation annot;
+		  	String mt;
+		   	annot = annotItr.next();
+		   	if (annot.getType().equals("Sentence")) {
+		   		annotSorted.add(annot);
+		   	}
+		   	
+		   	if (annot.getFeatures() == null || annot.getFeatures().get("majorType") == null) {
+		   		continue;
+		   	}
+		   	mt = annot.getFeatures().get("majorType").toString();
+		   	if (mt.equals("A") || mt.equals("B")) {
+		   		annotSorted.add(annot);
+		   	}
+		  }
+		  
+		  Annotation sentence = null;
+		  boolean hasA = false, hasB = false;
+		  for (Annotation a : annotSorted) {
+			  if (a.getType().equals("Sentence")) {
+				  if (sentence != null) {
+					  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {listA:" + hasA + ", listB:" + hasB + "}");
+				  }
+				  sentence = a;
+				  hasA = false;
+				  hasB = false;
+				  continue;
 			  }
-			  sentence = a;
-			  color = false;
-			  shape = false;
-			  continue;
+			  if (sentence == null) {
+				  continue;
+			  }
+			  if (a.getEndNode().getOffset() > sentence.getEndNode().getOffset()) {
+				  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {listA:" + hasA + ", listB:" + hasB + "}");
+				  sentence = null;
+				  continue;
+			  }
+			  String type;
+			  type = a.getFeatures().get("majorType").toString();
+			  if (type.equals("A")) {
+				  hasA = true;
+			  }
+			  if (type.equals("B")) {
+				  hasB = true;
+			  }
 		  }
-		  if (sentence == null) {
-			  continue;
+		  if (sentence != null) {
+			  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {listA:" + hasA + ", listB:" + hasB + "}");
 		  }
-		  if (a.getEndNode().getOffset() > sentence.getEndNode().getOffset()) {
-			  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {color:" + color + ", shape:" + shape + "}");
-			  sentence = null;
-			  continue;
-		  }
-		  String type;
-		  type = a.getFeatures().get("majorType").toString();
-		  if (type.equals("A")) {
-			  color = true;
-		  }
-		  if (type.equals("B")) {
-			  shape = true;
-		  }
-	  }
-	  if (sentence != null) {
-		  System.out.println("Sentence {" + sentence.getStartNode().getOffset() + "," + sentence.getEndNode().getOffset() + "} => {color:" + color + ", shape:" + shape + "}");
-	  }
-      Factory.deleteResource(doc);
-
+	      Factory.deleteResource(doc);
+	      System.out.println();
+      }
 
 
       System.out.println("done");
